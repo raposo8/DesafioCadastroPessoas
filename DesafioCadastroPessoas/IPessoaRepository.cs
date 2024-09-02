9eadf790f9ca6c706dab64dcbb0f6737aca99ed8
raposo8 <victinhorolim@gmail.com>
2024-09-02 20:46:12 -0300

    implement interface

---
+++ DesafioCadastroPessoas/IPessoaRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioCadastroPessoas
+{
+    
+        public interface IPessoaRepository
+        {
+            void AdicionarPessoa(Pessoa pessoa);
+            Pessoa BuscarPessoaPorId(int id);
+            void AtualizarPessoa(Pessoa pessoa);
+            void RemoverPessoa(int id);
+            void Conectar(); // Adicione se houver um método para conectar
+            void Desconectar(); // Adicione se houver um método para desconectar
+        }
+    
+}
